d2557f4f9b66274e938e42ba7cb9a46285b2dcd3
Amar <kajevic18@gmail>
2023-12-15 18:25:17 +0100

    Add project files.

---
+++ UseCases/ViewCategoriesUseCase.cs
@@ -0,0 +1,27 @@
+﻿using CoreBusiness;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UseCases.DataStorePluginInterfaces;
+using UseCases.UseCaseInterfaces;
+
+namespace UseCases
+{
+    public class ViewCategoriesUseCase : IViewCategoriesUseCase
+    {
+        private readonly ICategoryRepository _categoryRepository;
+
+        public ViewCategoriesUseCase(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+        public IEnumerable<Category> Execute()
+        {
+            return _categoryRepository.GetCategories();
+
+        }
+    }
+}
